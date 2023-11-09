e5c3cf5c396aac471f42fb20589f1bd9f68d0e44
dogukanma <blanksh364@gmail.com>
2023-11-09 19:54:21 +0300

    disabled form-handler.php

--- Eduaction-Website/form-handler.php
+++ Eduaction-Website/form-handler.php
@@ -1,26 +1,26 @@
 <?php
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+// $name = $_POST['name'];
+// $visitor_email = $_POST['email'];
+// $subject = $_POST['subject'];
+// $message = $_POST['message'];
 
-$email_from = "info@eduaction.com";
+// $email_from = "info@eduaction.com";
 
-$email_subject = "New from submission";
+// $email_subject = "New from submission";
 
-$email_body = "
-  Subject: $subject \n
-  Username: $name \n 
-  Email: $visitor_email \n
-  Message: $message
-";
+// $email_body = "
+//   Subject: $subject \n
+//   Username: $name \n 
+//   Email: $visitor_email \n
+//   Message: $message
+// ";
 
-$to = "yebov54614@eazenity.com";
+// $to = "";
 
-$headers = "From: $email_from \r\n";
-$headers .= "Reply to: $visitor_email \r\n";
+// $headers = "From: $email_from \r\n";
+// $headers .= "Reply to: $visitor_email \r\n";
 
-mail($to, $email_subject, $email_body, $headers);
-header("Location: contact.html")
+// mail($to, $email_subject, $email_body, $headers);
+// header("Location: contact.html")
 
 ?>
